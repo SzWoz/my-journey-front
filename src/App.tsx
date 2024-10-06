3175c5f3ba842d3fff31f6e205eec024c3dab321
Szymon Woźny <szwoz2001@gmail.com>
2024-10-06 15:56:37 +0200

    working

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { Tooltip } from "./components/ui/tooltip";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-full"></div>
+    </>
+  );
+}
+
+export default App;
