c1265fc46453084c4e20374bab2421ca3735cf20
Szymon Woźny <szwoz2001@gmail.com>
2024-10-06 18:58:40 +0200

    finalize lint config for tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,25 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import tailwind from 'eslint-plugin-tailwindcss';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  js.configs.recommended,
+  // add `typescript-eslint` flat config simply
+  // if you would like use more another configuration,
+  // see the section: https://typescript-eslint.io/getting-started#details
+  ...ts.configs.recommended,
+  ...tailwind.configs['flat/recommended'],
+
   {
-    ignores: ["src/components/ui/*", "tailwind.config.js"],
+    ignores: ['src/components/ui/*', 'tailwind.config.js'],
   },
 ];
